ec16b297087056a50d01aa5895fbcb7a1ed70dec
Dan <Reiuiji@gmail.com>
2013-04-18 20:56:58 -0400

    Fixed terminal output

--- main.cpp
+++ main.cpp
@@ -24,35 +24,35 @@ char const load_name[] = "test.Dat";
 
 void Verion()
 {
-    cerr << GameName << "  (compiled: " << __DATE__ << ")";
-    cerr << " Ver." << string(revision).substr(string(revision).find(" "), string(revision).rfind("$") - string(revision).find(" ")) //trick learned from EPIPHANY e-server, thank for the idea
+    cout << GameName << "  (compiled: " << __DATE__ << ")";
+    cout << " Ver." << string(revision).substr(string(revision).find(" "), string(revision).rfind("$") - string(revision).find(" ")) //trick learned from EPIPHANY e-server, thank for the idea
          << endl;
 
-    cerr << copyrigth << GameName << " Created for ECE264 Group Project(Spring 2013)\n";
-    cerr << "\n\033[4mPlease report bugs to:\033[0m \033[1m<reiuiji@gmail.com>\033[0m" << endl;
+    cout << copyrigth << GameName << " Created for ECE264 Group Project(Spring 2013)\n";
+    cout << "\n\033[4mPlease report bugs to:\033[0m \033[1m<reiuiji@gmail.com>\033[0m" << endl;
 } //\033 set default text and \033[1m makes it bold and \033[0m change back
 void Usage()
 {
-    cerr << "\033[4mUsage:\033[0m" << endl;
-    cerr << endl;
+    cout << "\033[4mUsage:\033[0m" << endl;
+    cout << endl;
 
-    cerr << GameName << " [--load <data-file>] [--debug] [--version] [--help]" << endl;
-    cerr << endl;
+    cout << GameName << " [--load <data-file>] [--debug] [--version] [--help]" << endl;
+    cout << endl;
 
-    cerr << "\033[4mProgram options:\033[0m" << endl;
-    cerr << endl;
+    cout << "\033[4mProgram options:\033[0m" << endl;
+    cout << endl;
 
-    cerr << " \033[1m--load   \033[0m" << "\t\tLoad a save file to the game" << endl;
+    cout << " \033[1m--load   \033[0m" << "\t\tLoad a save file to the game" << endl;
 
-    cerr << " \033[1m--debug  \033[0m" << "\t\tEnables Debug mode" << endl;
+    cout << " \033[1m--debug  \033[0m" << "\t\tEnables Debug mode" << endl;
 
-    cerr << " \033[1m--version\033[0m" << "\t\tDisplay the version number and copyrights" << endl;
+    cout << " \033[1m--version\033[0m" << "\t\tDisplay the version number and copyrights" << endl;
 
-    cerr << " \033[1m--help  \033[0m" << "\t\tDisplays the Help screen(you looking at it ;D)" << endl;
+    cout << " \033[1m--help  \033[0m" << "\t\tDisplays the Help screen(you looking at it ;D)" << endl;
 
-    cerr << endl;
-    cerr << "This program is covered by the GNU General Public License. See COPYING for" << endl;
-    cerr << "further information." << endl;
+    cout << endl;
+    cout << "This program is covered by the GNU General Public License. See COPYING for" << endl;
+    cout << "further information." << endl;
 }
 
 int main(int argc, char *argv[])
