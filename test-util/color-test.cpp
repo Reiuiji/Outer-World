f5c4a2d030ab0f4e3b6ed218c2b766e275151651
Daniel Noyes <reiuiji@gmail.com>
2013-04-19 23:42:54 -0400

    Input basics for a color test util

--- test-util/color-test.cpp
+++ test-util/color-test.cpp
@@ -17,6 +17,106 @@
 * You should have received a copy of the GNU General Public License
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+#include <curses.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+//color setup
+#define COLOR_BLACK	    0
+#define COLOR_RED	    1
+#define COLOR_GREEN	    2
+#define COLOR_YELLOW	3
+#define COLOR_BLUE	    4
+#define COLOR_MAGENTA	5
+#define COLOR_CYAN	    6
+#define COLOR_WHITE	    7
+#define COLOR_BROWN     8
+#define ENV_WATER       9
+#define ENV_WFALL       10//water fall
+#define ENV_PATH        11
+#define ENV_TREE        12
+
+void color_test(int Xpos, int Ypos, int Width, int Height, int ColorAStart, int ColorAEnd);
+void init_col();
+void init_col_2(short AStart, short AEnd);
+
+int main()
+{
+#ifdef PDCURSES
+    PDC_set_title(version);
+#endif
+
+//---------Initialize Curses---------
+
+    keypad(initscr(), 1);
+    keypad(stdscr, TRUE);
+    curs_set(0);
+    attron(COLOR_PAIR(COLOR_BLUE));
+    init_col_2(0,40);
+    while(1)
+    {
+        clear();
         //test color
-        attron(COLOR_PAIR(COLOR_BROWN));
-        mvaddstr(14,34, "Test");
+        init_col();
+        attron(COLOR_PAIR(COLOR_BLUE));
+        mvaddstr(0,0, "Press q to exit");
+        color_test(0,1,30,20,0,40);
+
+        int c = getch();
+        if(c == 'q')
+        {
+            exit(1);
+        }
+
+    }
+
+    return 0;
+}
+
+void color_test(int Xpos, int Ypos, int Width, int Height, int ColorAStart, int ColorAEnd)
+{
+    int x,y;
+    int A = ColorAStart;
+    for(y=Ypos; y<Height; y++)
+    {
+        for(x=Xpos; x<Width; x++)
+        {
+            attron(COLOR_PAIR(A));
+            mvaddstr(y,x,"#");
+            A++;
+            if(A>ColorAEnd)
+            {
+                A=ColorAStart;
+            }
+        }
+    }
+
+}
+
+void init_col()
+{
+    start_color();
+    init_pair(COLOR_BLACK, COLOR_BLACK, COLOR_BLACK);
+    init_pair(COLOR_GREEN, COLOR_GREEN, COLOR_BLACK);
+    init_pair(COLOR_RED, COLOR_RED, COLOR_BLACK);
+    init_pair(COLOR_CYAN, COLOR_CYAN, COLOR_BLACK);
+    init_pair(COLOR_WHITE, COLOR_WHITE, COLOR_BLACK);
+    init_pair(COLOR_BLUE, COLOR_BLUE, COLOR_BLACK);
+    init_pair(COLOR_YELLOW, COLOR_YELLOW, COLOR_BLACK);
+    init_color(COLOR_BROWN,800,520,245);
+    init_pair(ENV_WATER, COLOR_BLUE,COLOR_BLUE);
+    init_pair(COLOR_BROWN, COLOR_BROWN,COLOR_BLACK);
+    attron(COLOR_PAIR(COLOR_BLUE));
+};
+
+void init_col_2(short AStart, short AEnd)
+{
+    short A=0,B=0,C=0,D=0;
+    for(A=AStart;A<=AEnd;A++)
+    {
+        B=(A/AEnd)*1000;
+        init_color(A,B,C,D);
+    }
+
+}
